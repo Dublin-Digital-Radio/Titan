876a81f1a5f0186116a99a6084e87da362f433f1
Tom Shortall <tomshortall@gmail.com>
2023-02-13 18:44:14 +0000

    new class

--- discordbot/discordbot/bot.py
+++ discordbot/discordbot/bot.py
@@ -7,7 +7,6 @@ import discord
 
 # from raven import Client as RavenClient
 # import raven
-from aiohttp import web
 from config import config
 from redis.exceptions import ConnectionError
 
@@ -15,7 +14,7 @@ from discordbot import commands
 from discordbot.poststats import BotsDiscordPw, DiscordBotsOrg
 from discordbot.redisqueue import RedisQueue
 from discordbot.socketio import SocketIOInterface
-from discordbot.web import web_app
+from discordbot.web import Web
 
 # try:
 #     raven_client = RavenClient(config["sentry-dsn"])
@@ -55,7 +54,7 @@ def _handle_task_result(task: asyncio.Task) -> None:
         logging.exception("Exception raised by task = %r", task)
 
 
-class Titan(discord.AutoShardedClient):
+class Titan(discord.AutoShardedClient, Web):
     def __init__(self, shard_ids=None, shard_count=None):
         super().__init__(
             shard_ids=shard_ids,
@@ -79,8 +78,8 @@ class Titan(discord.AutoShardedClient):
 
         self.redis_sub_task = None
         self.post_stats_task = None
-        self.web_app = web.Application()
-        self.web_app.add_routes("/channel_messages/{channel_id}", self.http_get_channel_messages)
+
+        self.init_web()
 
     def _cleanup(self):
         try:
@@ -128,8 +127,6 @@ class Titan(discord.AutoShardedClient):
         if config["discord-bots-org-token"] or config["bots-discord-pw-token"]:
             self.post_stats_task = self.loop.create_task(self.auto_post_stats())
             self.post_stats_task.add_done_callback(_handle_task_result)
-
-        web.run_app(web_app)
 
     async def on_message(self, message):
         await self.socketio.on_message(message)
@@ -378,15 +375,3 @@ class Titan(discord.AutoShardedClient):
         shard_id = self.shard_id
         await self.discordBotsOrg.post(count, shard_count, shard_id)
         await self.botsDiscordPw.post(count, shard_count, shard_id)
-
-    async def http_get_channel_messages(self, request):
-        channel_id = request.match_info.get("channel_id")
-        channel = self.get_channel(int(channel_id))
-        if not channel or not isinstance(channel, discord.channel.TextChannel):
-            return
-        me = channel.guild.get_member(self.user.id)
-
-        messages = []
-        if channel.permissions_for(me).read_messages:
-            async for message in channel.history(limit=50):
-                messages.append(json.dumps(format_message(message), separators=(",", ":")))
--- discordbot/discordbot/web.py
+++ discordbot/discordbot/web.py
@@ -2,21 +2,30 @@ import discord
 from aiohttp import web
 
 
-async def handle(request):
-    name = request.match_info.get("name", "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+class Web(discord.AutoShardedClient):
+    def init_web(self):
+        self.web_app = web.Application()
 
+        self.web_app.add_routes([web.get("/", self.handle), web.get("/{name}", self.handle)])
+        self.web_app.add_routes(
+            ["/channel_messages/{channel_id}", self.http_get_channel_messages]
+        )
 
-async def on_get_channel_messages(request):
-    channel = self.bot.get_channel(int(channel_id))
-    if not channel or not isinstance(channel, discord.channel.TextChannel):
-        return
+        web.run_app(self.web_app)
 
+    async def handle(self, request):
+        name = request.match_info.get("name", "Anonymous")
+        text = "Hello, " + name
+        return web.Response(text=text)
 
-web_app = web.Application()
+    async def http_get_channel_messages(self, request):
+        channel_id = request.match_info.get("channel_id")
+        channel = self.get_channel(int(channel_id))
+        if not channel or not isinstance(channel, discord.channel.TextChannel):
+            return
+        me = channel.guild.get_member(self.user.id)
 
-web_app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
-
-if __name__ == "__main__":
-    web.run_app(web_app)
+        messages = []
+        if channel.permissions_for(me).read_messages:
+            async for message in channel.history(limit=50):
+                messages.append(json.dumps(format_message(message), separators=(",", ":")))
