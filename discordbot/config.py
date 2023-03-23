362d986d1ce5d31284adb66767c9b1142825be53
Tom Shortall <tomshortall@gmail.com>
2023-03-23 12:33:24 +0000

    remove unused config entry

--- discordbot/config.py
+++ discordbot/config.py
@@ -19,8 +19,5 @@ config = {
     "titan-web-app-secret": env["TITAN_WEBAPP_SECRET"],
     "discord-bots-org-token": env.get("DISCORDBOTS_POST_STATS_TOKEN"),
     "bots-discord-pw-token": env.get("BOTS.DISCORD.PW_POST_STATS_TOKEN"),
-    "logging-location": env.get(
-        "TITAN_WEBAPP_LOGGING_LOCATION", "/home/titan/titanbot.log"
-    ),
     "sentry-dsn": env.get("TITAN_WEBAPP_SENTRY_DSN", ""),
 }
